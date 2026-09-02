58253fed216333a3944017d12acb02cfdf29b9cd
zaydaansachedina-rgb <zaydaansachedina@gmail.com>
2026-09-02 12:53:25 -0400

    Working weather endpoint, though currently no database so wont be able to properply use it just proxying from terminal works

--- server.js
+++ server.js
@@ -23,6 +23,27 @@ app.get('/api/cities', async (req, res) => {
     }
 })
 
+
+app.get('/api/weather/:lat/:long', async (req, res) => {
+    const long = req.params.long
+    const lat = req.params.lat
+    const weatherUrl = `https://api.open-meteo.com/v1/forecast?latitude=${encodeURIComponent(lat)}&longitude=${encodeURIComponent(long)}&hourly=temperature_2m`
+    console.log(weatherUrl)
+    try { 
+        const anwser = await fetch(weatherUrl)
+        if (!anwser.ok) {
+            throw new Error(`Response status: ${anwser.status}`)
+        }
+        const data = await anwser.json()
+        const weather = data.hourly.temperature_2m[0]
+        res.json({weather})
+
+     } catch(error){
+        console.error(error.message)
+
+    }
+})
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`)
 })
